07c7d01a1fcbdb369e474d296ad80cd4495f2a07
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-09-13 14:47:57 +0200

    Update the ASCII art for 2 censored commands

--- nsfw.lua
+++ nsfw.lua
@@ -10,7 +10,7 @@ function furrybot.get_ascii_dick(name)
 end
 
 function furrybot.get_ascii_boobs(name)
-	return furrybot.get_ascii_genitals(name, "E", "Ξ", "B", 420)
+	return furrybot.get_ascii_genitals(name, "E", "Ξ", "3", 420)
 end
 
 function furrybot.commands.dicksize(name, target)
@@ -33,7 +33,7 @@ end)
 furrybot.commands.blowjob = furrybot.request_command(function(name, target)
 	furrybot.ping_message(target, name .. " wants to suck your dick. Type !accept to accept or !deny to deny.", furrybot.colors.system)
 end, function(name, target)
-	furrybot.send(name .. " is sucking " .. target .. "'s cock. ˣoˣ IT'S SO HUGE", furrybot.colors.roleplay)
+	furrybot.send(name .. " is sucking " .. target .. "'s cock. " .. furrybot.get_ascii_dick(name) .. " ˣoˣ ", furrybot.colors.roleplay)
 end)
 
 furrybot.commands.sex = furrybot.request_command(function(name, target)
